3da63c15afb2a9c0f64bc6421bdd54fd59fabeae
Amadhya Anand <35623760+Amadhya@users.noreply.github.com>
2021-02-12 19:31:20 +0530

    Update ArrayNesting.java

--- ArrayNesting.java
+++ ArrayNesting.java
@@ -22,3 +22,27 @@ public class ArrayNesting {
         return maxLen;
     }
 }
+
+class Solution2 {
+    public int arrayNesting(int[] nums) {
+        if (nums.length == 0)   return -1;
+
+        int maxLen = 0;
+
+        for(int num: nums) {
+            int len = -1;
+            int curr = num;
+
+            while(curr != -1) {
+                int temp = nums[curr];
+                nums[curr] = -1;
+                curr = temp;
+                len++;
+            }
+
+            maxLen = Math.max(maxLen, len);
+        }
+
+        return maxLen;
+    }
+}
