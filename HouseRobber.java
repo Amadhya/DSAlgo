0e5b9aa32a31bf046c40a4373b2a3ac9efa53e20
Amadhya Anand <35623760+Amadhya@users.noreply.github.com>
2021-04-11 14:14:31 +0530

    Update HouseRobber.java

--- HouseRobber.java
+++ HouseRobber.java
@@ -1,16 +1,8 @@
 class Solution {
     public int rob(int[] nums) {
         int len = nums.length;
-
-        if (len == 1) {
-            return nums[0];
-        }
-
-        if (len == 2) {
-            return Math.max(nums[0], nums[1]);
-        }
-
         int[] dp = new int[len + 1];
+        
         dp[0] = 0;
         dp[1] = nums[0];
 
